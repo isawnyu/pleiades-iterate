9ede6901fe2b609c6771e16418b10e7f53466e54
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-11-22 21:33:03 +0000

    Handle checkout as "admin"

--- pleiades/iterate/place.py
+++ pleiades/iterate/place.py
@@ -1,15 +1,30 @@
 #
 
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import (
+    newSecurityManager, setSecurityManager)
+from AccessControl.User import system
 from Products.CMFCore.utils import getToolByName
 
 def handleCheckout(event):
     # Change review state of all of a place's items to match the baseline
     wftool = getToolByName(event.object, "portal_workflow")
-    for oid, ob in event.working_copy.contentItems():
-        baseline = event.object[oid]
-        baseline_state = wftool.getInfoFor(baseline, 'review_state')
-        if baseline_state in ('pending', 'published'):
-            wftool.doActionFor(ob, action="submit")
-        if baseline_state in ('published'):
-            wftool.doActionFor(ob, action="publish")
+    mtool = getToolByName(event.object, "portal_membership")
+
+    # TODO: implement a new workflow for checked out items to replace
+    # this ugly security hack. Also note that checkouts expose 
+    sm = getSecurityManager()
+    admin = mtool.getMemberById("admin").getUser()
+    try:
+        newSecurityManager(None, admin.__of__(event.object.acl_users))
+        for oid, ob in event.working_copy.contentItems():
+            baseline = event.object[oid]
+            baseline_state = wftool.getInfoFor(baseline, 'review_state')
+            if baseline_state in ('pending', 'published'):
+                wftool.doActionFor(ob, action="submit")
+            if baseline_state in ('published'):
+                wftool.doActionFor(ob, action="publish")
+    except:
+        setSecurityManager(sm)
+        raise
 
