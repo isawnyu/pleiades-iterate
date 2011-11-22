fa198a9eec14650d42d95102d521c43726e86b57
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-11-22 21:33:05 +0000

    Better approach, create a temporary user with Manager role.

--- pleiades/iterate/place.py
+++ pleiades/iterate/place.py
@@ -1,10 +1,18 @@
 #
 
-from AccessControl import getSecurityManager
-from AccessControl.SecurityManagement import (
-    newSecurityManager, setSecurityManager)
-from AccessControl.User import system
+from AccessControl import ClassSecurityInfo, getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager, setSecurityManager
+from AccessControl.User import nobody
+from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
 from Products.CMFCore.utils import getToolByName
+
+class UnrestrictedUser(BaseUnrestrictedUser):
+    """Unrestricted user that still has an id.
+    """
+    def getId(self):
+        """Return the ID of the user.
+        """
+        return self.getUserName()
 
 def handleCheckout(event):
     # Change review state of all of a place's items to match the baseline
@@ -12,11 +20,15 @@ def handleCheckout(event):
     mtool = getToolByName(event.object, "portal_membership")
 
     # TODO: implement a new workflow for checked out items to replace
-    # this ugly security hack. Also note that checkouts expose 
+    # this hack. Also note that checkouts expose 
     sm = getSecurityManager()
-    admin = mtool.getMemberById("admin").getUser()
     try:
-        newSecurityManager(None, admin.__of__(event.object.acl_users))
+        tmp_user = UnrestrictedUser(
+            sm.getUser().getId(),
+            '', 
+            ['Manager'],
+            '')
+        newSecurityManager(None, tmp_user.__of__(event.object.acl_users))
         for oid, ob in event.working_copy.contentItems():
             baseline = event.object[oid]
             baseline_state = wftool.getInfoFor(baseline, 'review_state')
@@ -24,6 +36,7 @@ def handleCheckout(event):
                 wftool.doActionFor(ob, action="submit")
             if baseline_state in ('published'):
                 wftool.doActionFor(ob, action="publish")
+        setSecurityManager(sm)
     except:
         setSecurityManager(sm)
         raise
